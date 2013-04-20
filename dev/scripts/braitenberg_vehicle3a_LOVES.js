75aa4124df786ed265ee780318ddfd4087a4baf4
vinceallen <vince@lot18.com>
2013-04-20 16:52:53 -0400

    Updates all Braitenberg demos.

--- dev/scripts/braitenberg_vehicle3a_LOVES.js
+++ dev/scripts/braitenberg_vehicle3a_LOVES.js
@@ -1,34 +1,27 @@
 /*global Flora, Brait, document */
-
-var elements = function() {
-
-  'use strict';
+Flora.Mantle.System.create(function() {
 
   var i, max,
-      world = Flora.universe.first(),
-      maxVehicles = 5;
+      maxVehicles = 6,
+      getRandomNumber = Flora.Utils.getRandomNumber,
+      world = Flora.Mantle.System.allWorlds()[0];
 
-  //  update the universe
-  Flora.universe.update({
+  Flora.Mantle.World.update({
     c: 0.01,
     gravity: new Flora.Vector(),
-    width: 3000,
-    height: 1500,
+    width: Flora.Utils.getWindowSize().width / 0.45,
+    height: Flora.Utils.getWindowSize().height / 0.45,
     borderWidth: 1,
     borderStyle: 'solid',
-    borderColor: [100, 100, 100]
+    borderColor: [100, 100, 100],
+    color: [0, 0, 0]
   });
-
-  // create stimulants
-  for (i = 0, max = (0.015 * world.width); i < max; i += 1) {
-    Brait.Stimulus.create(null, new Flora.Vector(Flora.Utils.getRandomNumber(0, world.width),
-        Flora.Utils.getRandomNumber(0, world.height)), [Brait.Cold]);
-  }
 
   // create vehicles
   for (i = 0; i < maxVehicles; i += 1) {
 
     var vehicle = new Brait.Vehicle({
+      system: this,
       controlCamera: !i,
       color: !i ? [255, 255, 255] : [255, 100, 0],
       borderColor: !i ? [255, 100, 0] : [255, 150, 50],
@@ -42,16 +35,22 @@ var elements = function() {
     });
   }
 
+  // create stimulants
+  for (i = 0, max = (0.02 * world.bounds[1]); i < max; i += 1) {
+    Brait.Stimulus.create(null, new Flora.Vector(getRandomNumber(0, world.bounds[1]),
+        getRandomNumber(0, world.bounds[2])), [Brait.Cold]);
+  }
+
   // add event listener to create random stimulant on mouseup
   Flora.Utils.addEvent(document, 'mouseup', Brait.Stimulus.createCold);
 
-};
+}, null, null, true);
 
 Flora.Utils.addEvent(document.getElementById('buttonStart'), "mouseup", function(e) {
-  'use strict';
+
   if (e.stopPropagation) {
     e.stopPropagation();
   }
   document.getElementById('containerMenu').removeChild(document.getElementById('containerButton'));
-  Flora.System.start(elements);
+  Flora.Mantle.System.start();
 });
