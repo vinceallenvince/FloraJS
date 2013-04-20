f8afefe97f2cd4a124a6965f7e7e28b4f7b39873
vinceallen <vince@lot18.com>
2013-04-20 15:53:49 -0400

    Updates Light for v1.

--- src/light.js
+++ src/light.js
@@ -1,21 +1,19 @@
 /*global exports */
 /**
- * Creates a new Light object.
+ * Creates a new Light.
  *
  * @constructor
  * @extends Agent
  *
  * @param {Object} [opt_options] Options.
- * @param {number} [opt_options.mass = 50] Mass. Increase for a greater gravitational effect.
+ * @param {number} [opt_options.mass = 50] Mass.
  * @param {boolean} [opt_options.isStatic = true] If true, object will not move.
- * @param {number} [opt_options.width = 50] Width.
- * @param {number} [opt_options.height = 50] Height.
- * @param {number} [opt_options.opacity = 0.85] Opacity.
+ * @param {number} [opt_options.width = 100] Width.
+ * @param {number} [opt_options.height = 100] Height.
+ * @param {number} [opt_options.opacity = 0.75] Opacity.
  * @param {number} [opt_options.zIndex = 10] The object's zIndex.
  */
 function Light(opt_options) {
-
-  'use strict';
 
   var options = opt_options || {};
 
@@ -25,8 +23,10 @@ function Light(opt_options) {
   this.isStatic = options.isStatic === false ? false : options.isStatic || true;
   this.width = options.width === 0 ? 0 : options.width || 50;
   this.height = options.height === 0 ? 0 : options.height || 50;
-  this.opacity = options.opacity === 0 ? 0 : options.opacity || 0.85;
+  this.opacity = options.opacity === 0 ? 0 : options.opacity || 0.75;
   this.zIndex = options.zIndex === 0 ? 0 : options.zIndex || 10;
+
+  exports.Mantle.PubSub.publish('UpdateCache', this);
 }
 exports.Utils.extend(Light, exports.Agent);
 
