335bc52bc13470ff4d228dd1ca7b1b9364db91eb
vinceallen <vince@lot18.com>
2013-04-20 15:55:43 -0400

    Updates Oxygen for v1.

--- src/oxygen.js
+++ src/oxygen.js
@@ -1,21 +1,19 @@
 /*global exports */
 /**
- * Creates a new Oxygen object.
+ * Creates a new Oxygen.
  *
  * @constructor
  * @extends Agent
  *
  * @param {Object} [opt_options] Options.
- * @param {number} [opt_options.mass = 50] Mass. Increase for a greater gravitational effect.
+ * @param {number} [opt_options.mass = 50] Mass.
  * @param {boolean} [opt_options.isStatic = true] If true, object will not move.
- * @param {number} [opt_options.width = 50] Width.
- * @param {number} [opt_options.height = 50] Height.
- * @param {number} [opt_options.opacity = 0.75] The particle's opacity.
+ * @param {number} [opt_options.width = 100] Width.
+ * @param {number} [opt_options.height = 100] Height.
+ * @param {number} [opt_options.opacity = 0.75] Opacity.
  * @param {number} [opt_options.zIndex = 10] The object's zIndex.
  */
 function Oxygen(opt_options) {
-
-  'use strict';
 
   var options = opt_options || {};
 
@@ -27,6 +25,8 @@ function Oxygen(opt_options) {
   this.height = options.height === 0 ? 0 : options.height || 50;
   this.opacity = options.opacity === 0 ? 0 : options.opacity || 0.75;
   this.zIndex = options.zIndex === 0 ? 0 : options.zIndex || 10;
+
+  exports.Mantle.PubSub.publish('UpdateCache', this);
 }
 exports.Utils.extend(Oxygen, exports.Agent);
 
