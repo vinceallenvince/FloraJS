1a38a70ad48343f80f86440198b3cd94639b02ff
Vince Allen <vince@spotify.com>
2014-06-30 12:15:28 -0400

    Removes release, specs folder.

--- specs/Config.js
+++
@@ -1,9 +0,0 @@
-describe("config", function() {
-
-  var config = exports.config;
-
-  it("it should have its required properties.", function() {
-    expect(typeof config.keyMap).toEqual('object');
-    expect(typeof config.touchMap).toEqual('object');
-  });
-});
