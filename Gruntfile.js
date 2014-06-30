81f78269731d0555907ff51c281fd311a0d77047
Vince Allen <vince@spotify.com>
2014-06-30 12:17:18 -0400

    Updates Gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
 
-  var name, latest, bannerContent, bannerContentMin, footerContent,
-      publicRelease, lDevRelease, lMinRelease;
+  var name, latest, bannerContent, bannerContentMin, footerContent, devRelease;
 
   latest = '<%= pkg.name %>';
   name = '<%= pkg.name %>-v<%= pkg.version%>';
@@ -25,12 +24,16 @@ module.exports = function(grunt) {
 
   footerContent = '\n}(exports));';
 
-  lDevRelease = 'release/' + latest + '.js';
-  lMinRelease = 'release/' + latest + '.min.js';
-  publicRelease = 'public/scripts/' + latest + '.min.js';
+  devRelease = 'release/' + latest + '.js';
 
   grunt.initConfig({
     pkg : grunt.file.readJSON('package.json'),
+    browserify: {
+      client: {
+        src: ['src/**/*.js'],
+        dest: devRelease
+      }
+    },
     jshint: {
       options: {
         jshintrc: '.jshintrc'
@@ -44,9 +47,6 @@ module.exports = function(grunt) {
         csslintrc: '.csslintrc'
       },
       lax: {
-        options: {
-          import: false
-        },
         src: ['css/*.css']
       }
     },
@@ -59,82 +59,42 @@ module.exports = function(grunt) {
         dest: 'release/' + latest + '.min.css'
       }
     },
-    concat: {
-      options: {
-        banner: bannerContent,
-        footer: footerContent,
-        stripBanners: true,
-        process: function(src, filepath) {
-          if (filepath === 'src/raf.js') {
-            return src;
-          } else {
-            var className = filepath.replace('src/', '').replace('.js', '');
-            return src + '\nexports.' + className + ' = ' + className + ';\n';
-          }
-        }
-      },
-      target: {
-        src: ['src/Config.js', 'src/Interface.js', 'src/Utils.js', 'src/SimplexNoise.js', 'src/BorderPalette.js', 'src/ColorPalette.js',
-            'src/ColorTable.js', 'src/Caption.js', 'src/InputMenu.js', 'src/Mover.js', 'src/Agent.js', 'src/Walker.js', 'src/Sensor.js',
-            'src/Connector.js', 'src/Point.js', 'src/Particle.js', 'src/ParticleSystem.js', 'src/Oscillator.js', 'src/Dragger.js',
-            'src/Attractor.js', 'src/Repeller.js', 'src/Stimulus.js', 'src/FlowField.js', 'src/FlowFieldMarker.js', 'src/RangeDisplay.js'],
-        dest: 'release/' + latest + '.js'
-      }
-    },
     uglify: {
       options: {
         banner: bannerContentMin,
-        mangle: true,
-        compress: true,
-        wrap: latest,
-        exportAll: true,
-        report: 'min'
+        report: 'min',
+        sourceMap: true
       },
       target: {
-        src: ['src/Config.js', 'src/Interface.js', 'src/Utils.js', 'src/SimplexNoise.js', 'src/BorderPalette.js', 'src/ColorPalette.js',
-            'src/ColorTable.js', 'src/Caption.js', 'src/InputMenu.js', 'src/Mover.js', 'src/Agent.js', 'src/Walker.js', 'src/Sensor.js',
-            'src/Connector.js', 'src/Point.js', 'src/Particle.js', 'src/ParticleSystem.js', 'src/Oscillator.js', 'src/Dragger.js',
-            'src/Attractor.js', 'src/Repeller.js', 'src/Stimulus.js', 'src/FlowField.js', 'src/FlowFieldMarker.js', 'src/RangeDisplay.js'],
+        src: ['release/' + latest + '.js'],
         dest: 'release/' + latest + '.min.js'
       }
     },
     copy: {
-      versionDev: {
-        src: 'release/' + latest + '.js',
-        dest: 'release/versions/' + name + '.js'
-      },
-      versionMinified: {
-        src: 'release/' + latest + '.min.js',
-        dest: 'release/versions/' + name + '.min.js'
-      },
-      versionCSS: {
-        src: 'release/' + latest + '.min.css',
-        dest: 'release/versions/' + name + '.min.css'
-      },
-      publicDev: {
-        src: lDevRelease,
-        dest: publicRelease
-      },
-      publicMin: {
-        src: lMinRelease,
-        dest: publicRelease
+      publicJS: {
+        expand: true,
+        cwd: 'release/',
+        src: '*.js',
+        dest: 'public/scripts/',
+        flatten: true,
+        filter: 'isFile'
       },
       publicCSS: {
-        src: 'release/' + latest + '.min.css',
-        dest: 'public/css/' + latest + '.min.css'
+        expand: true,
+        cwd: 'release/',
+        src: '*.css',
+        dest: 'public/css/',
+        flatten: true,
+        filter: 'isFile'
       }
+    },
+    exec: {
+      test: 'npm test',
+      testcoverage: 'browserify -t coverify test/*.js | testling | coverify'
     },
     watch: {
       files: ['src/*.js'],
       tasks: ['jshint'],
-    },
-    jasmine: {
-      src: 'src/*.js',
-      options: {
-        version: '1.3.0',
-        specs: 'specs/*.js'
-      }
-
     },
     plato: {
       options: {},
@@ -154,22 +114,26 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-exec');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-plato');
   grunt.loadNpmTasks('grunt-jsdoc');
+  grunt.loadNpmTasks('grunt-browserify');
 
-  grunt.registerTask('default', ['cssmin', 'concat', 'copy:publicDev', 'copy:publicCSS']);
-  grunt.registerTask('release', ['jshint', 'cssmin', 'concat', 'uglify', 'copy:publicMin', 'copy:publicCSS', 'copy:versionCSS', 'copy:versionMinified', 'copy:versionDev', 'plato', 'jsdoc']);
-  grunt.registerTask('test', ['jshint', 'jasmine']);
-  grunt.registerTask('lint', ['jshint']);
+  grunt.registerTask('default', ['cssmin', 'browserify', 'copy:publicJS', 'copy:publicCSS']);
+  grunt.registerTask('release', ['csslint', 'jshint', 'cssmin', 'browserify', 'uglify', 'copy:publicJS', 'copy:publicCSS', 'jsdoc', 'plato']);
+  grunt.registerTask('test', ['exec:test']);
+  grunt.registerTask('testcoverage', ['exec:testcoverage']);
   grunt.registerTask('report', ['plato']);
   grunt.registerTask('doc', ['jsdoc']);
+  grunt.registerTask('lint', ['csslint', 'jshint']);
+
+  // node node_modules/testling/node_modules/browser-launcher/example/detect.js
+
 };
 
